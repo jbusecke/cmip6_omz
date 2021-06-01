07a2e2cb46a7e785bee632fdbf84fdf87b0ffb25
Sam J. Ditkovsky <samjd@princeton.edu>
2021-06-01 16:52:08 -0400

    tests for cmip6_omz.units

---
+++ tests/test_units.py
@@ -0,0 +1,27 @@
+import pytest
+import numpy as np
+import xarray as xr
+from cmip6_omz.units import (
+    convert_o2_ml_l,
+    convert_mol_m3_mymol_kg,
+)
+
+###Tests for cmip6-omz.units###
+
+@pytest.fixture
+def dummy_o2_ds():
+    # create dummy dataset
+    ds = xr.DataArray(np.random.rand(4)).to_dataset(name="dummy")
+    ds.attrs = {"units": "dummy unit"}
+    return ds
+
+def test_convert_o2_ml_l(dummy_o2_ds):
+    ds = convert_o2_ml_l(dummy_o2_ds)
+    assert ds.attrs["units"] == r"$mol/kg$"
+    assert (ds.dummy == dummy_o2_ds.dummy * 43.570 / 1e6).all()
+
+def test_convert_mol_m3_mymol_kg(dummy_o2_ds):
+    rho_0 = 1025.
+    ds = convert_mol_m3_mymol_kg(dummy_o2_ds)
+    assert ds.attrs["units"] == r"$\mu mol/kg$"
+    assert (ds.dummy == dummy_o2_ds.dummy / rho_0 * 1e6).all()
