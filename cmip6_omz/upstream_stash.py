bb144c89d0c287a1254bad52b3e83eb7e6328772
Sam J. Ditkovsky <samjd@princeton.edu>
2021-06-23 15:25:55 -0400

    move match_and_detrend to upstream.py

--- cmip6_omz/upstream_stash.py
+++ cmip6_omz/upstream_stash.py
@@ -4,6 +4,8 @@ from xgcm import Grid
 import cf_xarray
 import warnings
 
+from cmip6_preprocessing.drift_removal import remove_trend
+
 def sigma_spacing():
     """Return a fine global density (sigma_0) spacing"""
     dsigma = 0.05
@@ -147,3 +149,79 @@ def xgcm_transform_wrapper(ds, extensive_vars=[], target=None, target_data=None)
     if target_name + "_bounds" not in transformed_ds.coords:
         raise RuntimeError("Implement this thing before saving any more output!")
     return transformed_ds
+
+
+####function to cmip6_pp#########
+
+
+#Helper functions for match_and_detrend()
+#but may also be useful elsewhere
+def construct_cfdate(data, units, calendar):
+    date = xr.DataArray(data, attrs={"units": units, "calendar": calendar})
+    return xr.decode_cf(date.to_dataset(name="time"), use_cftime=True).time
+
+def _get_calendar(time):
+    ### This assumes that the time is decoded already
+    return time.encoding.get("calendar", "standard")
+
+
+def match_and_detrend(data_dict, trend_dict, pass_variables=[], verbose=False):
+    """
+    Takes CMIP6 datasets dict and a dictionary of trend datasets, and returns the detrended datasets.
+    Datasets with variable in `pass_variables` are passed through unaltered.
+    """
+    sep='_'
+    
+    compare_attrs = [ #should version_id also be here?
+        "source_id",
+        "table_id",
+        "grid_label",
+        "variant_label",
+        "variable_id",
+    ]
+
+
+    data_dict_detrended = {}
+    for name, ds in data_dict.items():
+        ds = ds.copy()
+        if ds.attrs["variable_id"] in pass_variables:
+            if verbose:
+                print(f"Manually Ignored Variable: Passing {name} unaltered")
+            data_dict_detrended[name] = ds
+        elif ds.attrs["experiment_id"] == "piControl": 
+            if verbose:
+                print(f"Control run: Passing {name} unaltered")
+            data_dict_detrended[name] = ds
+        else:
+            match_elements = [ds.attrs[i] for i in compare_attrs]
+            match_targets = list(trend_dict.keys()) #use attr once trend files preserve them
+            match_trend_names = [
+                m for m in match_targets if all([me+sep in m for me in match_elements])
+            ]
+            if len(match_trend_names) == 1:
+                trend_ds = trend_dict[match_trend_names[0]]
+
+                ref_date = construct_cfdate(
+                    0, "hours since 1850-1-15", _get_calendar(ds.time)
+                ).data.tolist()
+                
+                #should this be handled elsewhere?
+                if "slope" in trend_ds.variables:
+                    trend_ds = trend_ds.rename({"slope":ds.attrs["variable_id"]})
+                
+                da_detrended = remove_trend(
+                    ds, 
+                    trend_ds,
+                    ds.attrs["variable_id"],
+                    ref_date=ref_date
+                )
+                ds[ds.attrs["variable_id"]] = da_detrended.reset_coords(drop=True)
+                data_dict_detrended[name] = ds
+                
+                ds.attrs["detrended_with_file"] = 'Done'
+                
+            elif len(match_trend_names) > 1:
+                raise ValueError(f"Found multiple matches for {match_elements}: {match_trend_names}. Check input")
+            else:
+                warnings.warn(f"No match found for {match_elements}.")
+    return data_dict_detrended
