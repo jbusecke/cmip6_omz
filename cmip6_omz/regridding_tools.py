36c219a9868fb01b2602bccf0941431cb74b8b3a
Julius Busecke <julius@ldeo.columbia.edu>
2021-07-27 10:53:56 -0400

    added oxycline notebooks and some utilites (#34)
    * added oxycline notebooks and some utilites
    
    * Delete _version.py
    
    Co-authored-by: Julius Busecke <jbusecke@princeton.edu>

---
+++ cmip6_omz/regridding_tools.py
@@ -0,0 +1,10 @@
+import xesmf as xe
+import xarray as xr
+
+
+def regrid_regular(ds, spacing=0.5):
+    target_grid = xe.util.grid_global(spacing, spacing)
+    # this makes problems whan passing a dataarray. 
+    # find a better way to drop everythig but lon/lat
+    regridder = xe.Regridder(ds.drop([v for v in ds.variables if v not in ['lon', 'lat']]), target_grid, 'bilinear', periodic=True, ignore_degenerate=True)
+    return regridder(ds)
