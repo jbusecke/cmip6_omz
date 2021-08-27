fa2da32428cd032b7deac6457217fb3a3604940c
Julius Busecke <julius@ldeo.columbia.edu>
2021-08-27 12:39:12 -0400

    Added unit conversion for mg/l oxygen (#38)

--- tests/test_units.py
+++ tests/test_units.py
@@ -4,6 +4,7 @@ import xarray as xr
 from cmip6_omz.units import (
     convert_o2_ml_l,
     convert_mol_m3_mymol_kg,
+    convert_mg_l_to_mymol_kg
 )
 
 ###Tests for cmip6-omz.units###
@@ -25,3 +26,10 @@ def test_convert_mol_m3_mymol_kg(dummy_o2_ds):
     ds = convert_mol_m3_mymol_kg(dummy_o2_ds)
     assert ds.attrs["units"] == r"$\mu mol/kg$"
     assert (ds.dummy == dummy_o2_ds.dummy / rho_0 * 1e6).all()
+
+def test_convert_mg_l_to_mymol_kg(dummy_o2_ds):
+    rho_0 = 1025.
+    ds = convert_mg_l_to_mymol_kg(dummy_o2_ds)
+    assert ds.attrs["units"] == r"$\mu mol/kg$"
+    assert (ds.dummy == dummy_o2_ds.dummy * 1/32000 * rho_0/1000 * 1e6).all()
+
